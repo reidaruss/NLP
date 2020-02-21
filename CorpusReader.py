502b7c6795c524d380115db54ceb2776a3275116
reidaruss <reidarussell@gmail.com>
2020-02-20 20:07:35 -0600

    Finished tf-idf and all variations of constructor. also improved speed by allot so can do more than half the dataset in under a minute. still need to methods

--- CorpusReader.py
+++ CorpusReader.py
@@ -4,6 +4,7 @@ import nltk
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import copy
 
 class CorpusReader_TFIDF:
     def __init__(self, corpus, tf = "raw", idf = "base", stopword = "english", stemmer = "porter", ignorecase = "ignore"):
@@ -30,7 +31,14 @@ class CorpusReader_TFIDF:
             print("Invalid stopword parameter.")
             return
 
-        # Calculate tf #######
+        if idf.lower() == "base" or idf.lower() == "smooth":
+            self.idf = idf.lower()
+        else:
+            print("Invalid idf parameter")
+            return
+
+
+        # Calculate tf-idf #######
         if tf.lower() == "raw" or tf.lower() == "log" or tf.lower() == "binary": #  Use log normalized term frequency
             self.tf = tf.lower()
             self.ptf = self.tf_calc(self.tf) # gets dataframe of docs, unique words and their frequency
@@ -39,17 +47,13 @@ class CorpusReader_TFIDF:
             return
 
         # Calculate idf #######
-        if idf.lower() == "base" or idf.lower() == "smooth":
-            self.idf = idf.lower()
-        else:
-            print("Invalid idf parameter")
-            return
 
         return
 
     def tf_calc(self, tfType):
-        scale_factor = 0.05 # Look at a smaller dataset
-
+        scale_factor = 0.5 # Look at a smaller dataset
+        N = round(self.df[1].size * scale_factor)
+        tf_idf = []
         total_unique = np.array([]) # A np array that tracks all unique words
         freqs = []  # Tracks the dictionaries associated with each document
         for doc in range(round(self.df[1].size*scale_factor)):
@@ -63,6 +67,7 @@ class CorpusReader_TFIDF:
             if self.stopwords != "NULL":     # Remove stopwords
                 a = [w for w in a if not w in self.stopwords]
 
+
             unique, counts = np.unique(a, return_counts = True)     # Get the unique values and their counts
             if tfType == 'log':
                 counts = 1 + (np.log(counts) / np.log(2))
@@ -70,23 +75,29 @@ class CorpusReader_TFIDF:
                 counts = np.ones(len(counts))
             tempdict = dict(zip(unique,counts))   # Put unique vals and counts into dictionary
 
-            total_unique = np.unique(np.concatenate([total_unique,unique]))   # Create a list of unique words for all docs
+            total_unique = np.concatenate([total_unique,unique])   # Create a list of unique words for all docs
             freqs.append(tempdict)
-            if doc % 5 == 0 and doc != 0:
+            if doc % 50 == 0 and doc != 0:
                 print((doc/round(self.df[1].size*scale_factor))*100)
+        total_unique = np.unique(total_unique)
+        ni = np.zeros(len(total_unique))
+        for i in range(len(total_unique)):
+            for subdict in freqs:
+                if total_unique[i] in subdict:
+                    ni[i] = ni[i] + 1
 
 
-        return_df = pd.DataFrame(0, index=self.docs[:round(self.df[1].size*scale_factor)], columns=sorted(total_unique))  # Create dataframe with all unique
-        return_dict = {}
-        for doc in range(round(self.df[1].size*scale_factor)):
-            return_dict[self.docs[doc]] = freqs[doc]  # Asociate each document with its dictionary of unique words and freqs
 
-        for doc, tdict in return_dict.items():  # Fill dataframe with each frequency by doc and word in dictionary
-            tdict = dict(tdict)
-            for word, freq in tdict.items():
-                return_df.loc[doc, word] = freq
+        for i in range(len(freqs)):
+            tfidf = copy.deepcopy(freqs[i])
+            for j in freqs[i].items():
+                if self.idf == 'smooth':
+                    tfidf[j[0]] =  j[1] * (np.log(1+(N/ni[np.where(total_unique == j[0])]))/np.log(2))
+                else:
+                    tfidf[j[0]] = j[1] * (np.log(N / ni[np.where(total_unique == j[0])]) / np.log(2))
+            tf_idf.append(tfidf)
 
-        return return_df
+        return tf_idf
 
     def getTF(self):
         return self.ptf
--- program1.py
+++ program1.py
@@ -6,13 +6,19 @@ brown = nltk.corpus.brown
 sotu = nltk.corpus.state_union
 
 CR1 = CorpusReader.CorpusReader_TFIDF(brown)
-CR2 = CorpusReader.CorpusReader_TFIDF(sotu)
+#CR2 = CorpusReader.CorpusReader_TFIDF(sotu)
 
 df1 = CR1.getTF()
-df2 = CR2.getTF()
+#df2 = CR2.getTF()
 
-print(df1.describe())
-print(df2.describe())
+print("BROWN::::::::::")
+for x in df1:
+    print(x)
+print(" ")
+print(len(df1))
+# print("SOTU::::::::::::")
+# for x in df2:
+#     print(x)
 
 
 #print(df1.loc[])
